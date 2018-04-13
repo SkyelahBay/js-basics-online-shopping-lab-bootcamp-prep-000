1464534ca41bbc3a794448e874340c39d5f5a88b
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-13 01:33:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,11 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i = 0; i < cart.length; i++){
+    if (cart[i].itemName === item){
+      cart.splice()
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
